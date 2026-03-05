252ccfb72e2a75ac97a40b44442355862e4c6ee2
Vans Mcklyed Gonzales <vanzmckly@gmail.com>
2026-03-05 18:59:46 +0800

    Finished

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.use(express.json());
+
+//Serve static file from the "public" folder
+app.use(express.static('public'));
+
 //Define a route for the home page
 app.get('/', (req, res) => {
     res.send('Hello, World!');
@@ -10,4 +25,26 @@ app.get('/', (req, res) => {
 //Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Us');
+});
+
+app.post('/submit', (req, res) => {
+    const data = req.body;
+    res.send(`Received: ${JSON.stringify(data)}`);
+});
+
+const items = ['Apple', 'Banana', 'Orange'];
+
+app.get('/items', (req, res) => {
+    res.json(items);
+});
+
+
+app.post('/items', (req, res) => {
+    const newItem = req.body.item;
+    items.push(newItem);
+    res.json(items);
 });
